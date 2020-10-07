9aa6658b197934225c959080ba7872e4ae5722d0
rashad <rashad.arbab@gmail.com>
2020-10-07 15:24:18 -0400

    Updates

---
+++ precache-manifest.a1abe60b23d60a6d0a3ff7af936e13d2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d520ed7f42b9d5b13dfe7d1c38774e06",
+    "url": "/http//:RashadArbab.github.io/ToDoList/index.html"
+  },
+  {
+    "revision": "37ec475932c4cda2e40f",
+    "url": "/http//:RashadArbab.github.io/ToDoList/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "3104a885060a4abf8039",
+    "url": "/http//:RashadArbab.github.io/ToDoList/static/css/main.cf1e45ec.chunk.css"
+  },
+  {
+    "revision": "37ec475932c4cda2e40f",
+    "url": "/http//:RashadArbab.github.io/ToDoList/static/js/2.01d7a7cd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/http//:RashadArbab.github.io/ToDoList/static/js/2.01d7a7cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3104a885060a4abf8039",
+    "url": "/http//:RashadArbab.github.io/ToDoList/static/js/main.9e8fcfa1.chunk.js"
+  },
+  {
+    "revision": "315350c3029eeb536180",
+    "url": "/http//:RashadArbab.github.io/ToDoList/static/js/runtime-main.a8eaf567.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/http//:RashadArbab.github.io/ToDoList/static/media/logo.5d5d9eef.svg"
+  }
+]);
